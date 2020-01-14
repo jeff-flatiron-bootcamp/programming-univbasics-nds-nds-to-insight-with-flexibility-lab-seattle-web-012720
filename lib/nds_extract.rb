7c5872aede850733c29868d275bdafb5d925d335
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-14 03:27:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -92,12 +92,12 @@ def gross_per_studio(collection)
       worldwide_gross = collection[director_index][:title][:worldwide_gross]
       
       puts "Hello"#"Converting an item: #{collection[director_index]}"
-      puts #" Subitem title Hash: #{collection[director_index][:title]}"
-      puts #" Subitem title string: #{title}"
-      puts #" Subitem title studio: #{studio}"
-      puts #" Subitem title worldwide_gross: #{worldwide_gross}"
+      puts "Hello"#" Subitem title Hash: #{collection[director_index][:title]}"
+      puts "Hello"#" Subitem title string: #{title}"
+      puts "Hello"#" Subitem title studio: #{studio}"
+      puts "Hello"#" Subitem title worldwide_gross: #{worldwide_gross}"
       temp_hash = {:title => title, :studio => studio, :worldwide_gross =>worldwide_gross}
-      puts #" Prepped hash: #{temp_hash}"
+      puts "Hello"#" Prepped hash: #{temp_hash}"
       process_collection.push(temp_hash)
       director_index+=1
     end
